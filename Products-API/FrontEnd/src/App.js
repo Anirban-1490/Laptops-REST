a485291ae54c4beae73303695f2d81183cf1460f
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-03-13 21:08:20 +0530

    FrontEnd with Read

---
+++ Products-API/FrontEnd/src/App.js
@@ -0,0 +1,10 @@
+import {Main} from "./Components/Main"
+
+function App() {
+    return <>
+    <Main/>
+    </>
+
+}
+
+export default App;
---
+++ Products-API/FrontEnd/src/Components/Main.js
@@ -0,0 +1,57 @@
+
+import React,{useEffect,useState} from 'react';
+import "../Styling/Main_Style.css"
+import axios from 'axios'
+
+export const Main = ()=>
+{
+    const [windowSize,setWindowSize] = useState(window.innerWidth)
+    const [data,setData] = useState([])
+   const URL_GET_DATA = "http://localhost:5000/api/v1"
+    
+
+    useEffect(()=>{
+        async function fetchData(){
+            const result = await axios.get(URL_GET_DATA)
+            setData(res=>[...res,...result.data.data])
+        }
+      
+        fetchData()
+    },[URL_GET_DATA])
+
+    const callbc = ()=>{
+        console.log(windowSize);
+        setWindowSize(window.innerWidth)
+    }
+
+    useEffect(()=>{
+        window.addEventListener("resize",callbc)
+        return ()=> window.removeEventListener("resize",callbc)
+    },[windowSize])
+
+    return <>
+        <div className='container-main'>
+            <h2>Laptop-ssss</h2>
+            <main>
+               {
+                      
+                   data && data.map(item=>{
+                    
+                    const {img,name,price,rating,_id} = item;
+
+                    return <div key={_id} className ="item-container">
+                        <div className='img-container'><img src={img} alt="image here" /></div>
+                        <div className='info'>
+                            <h3>{(windowSize<=430)?String(name).substring(0,50)+"...":(windowSize<=810)?String(name).substring(0,90)+"...":name}</h3>
+                            <div>
+                                <h4> &#8377;{price}</h4>
+                                <h4><ion-icon name="star"></ion-icon>{rating}</h4>
+                            </div>
+                        </div>
+                    </div>
+                   })
+               }
+            </main>
+        </div>
+    </>
+}
