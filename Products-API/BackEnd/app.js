71b6c1aceaf0d2cfa955a629f4a3eed607c9cbe0
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-03-13 20:55:58 +0530

    added Axios and CORS

--- Products-API/BackEnd/app.js
+++ Products-API/BackEnd/app.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express()
 const router  = require("./model/router")
 const connectDB =  require("./Database/db_connection");
+const cors = require("cors")
 
 app.use(express.urlencoded())
+app.use(cors()) //*middleware for cors issue
 app.use(express.json())
 app.use("/api/v1",(req,res,next)=>{
     console.log("connected");
