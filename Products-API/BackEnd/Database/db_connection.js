c60cd12b894dc3579a1c3c3d4dec4816407d33f9
Anirban-1490 <anirbanpratihar45@gmail.com>
2022-03-18 13:14:31 +0530

    improved code readability

--- Products-API/BackEnd/Database/db_connection.js
+++ Products-API/BackEnd/Database/db_connection.js
@@ -1,8 +1,7 @@
 
 const mongoose = require("mongoose");
+require('dotenv').config()
 
-const password = encodeURIComponent("@Anirban45#")
-const anotherpaa = encodeURIComponent("JvPEUo7FEwfI8UVO");
 
 
 
@@ -10,7 +9,7 @@ const anotherpaa = encodeURIComponent("JvPEUo7FEwfI8UVO");
 
 const connectDB = ()=>
 {
-    return mongoose.connect(`mongodb+srv://Anirban45:${anotherpaa}@cluster-anirban.palk8.mongodb.net/LAPTOP-API-DB?retryWrites=true&w=majority`,{socketTimeoutMS:120000})
+    return mongoose.connect(process.env.CONNECTION_STRING,{socketTimeoutMS:120000})
 }
 
 module.exports = connectDB;
